8139968d83c69d9efdf949ba816e7678d477e3ec
FrancoRiveiro <franco.riveiro2@gmail.com>
2025-07-03 14:27:12 -0300

    Transporte y Reserva
    Solo estan creadas con alguna propiedad, no tienen ninguna funcionalidad

---
+++ Trabajo Final p1/Clases/MedioDeTransporte.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_Final_p1.Clases
+{
+    abstract class MedioDeTransporte
+    {
+		private int _valorKM;
+
+		public int ValorKM
+		{
+			get { return _valorKM; }
+			set { _valorKM = value; }
+		}
+
+		private int _capacidad;
+
+		public int Capacidad
+		{
+			get { return _capacidad; }
+			set { _capacidad = value; }
+		}
+	}
+}
---
+++ Trabajo Final p1/Clases/Reserva.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_Final_p1.Clases
+{
+    public class Reserva
+    {
+
+		private int idReserva;
+
+		public int IdReserva
+		{
+			get { return idReserva; }
+			set { idReserva = value; }
+		}
+
+		private int _cupos;
+
+		public int Cupos
+		{
+			get { return _cupos; }
+			set { _cupos = value; }
+		}
+
+        public List<Cliente> clientes { get; set; }
+
+
+    }
+}
